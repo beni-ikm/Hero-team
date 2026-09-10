ce0549c0834d6d6a94566d50eed1184856c1960b
Manuel Marcelo Baechle <manuelmarcelobachle@gmail.com>
2026-09-10 17:14:16 -0400

    Ajout de robe pour equipement pour le magicien

--- src/classes/Job.java
+++ src/classes/Job.java
@@ -7,7 +7,7 @@ public enum Job {
 
     WARRIOR(12, 0, List.of("Long Sword", "Chainmail Armor")),
     RANGER(10, 0, List.of("Bow", "Leather Armor")),
-    MAGE(6, 10, List.of("Wand")),
+    MAGE(6, 10, List.of("Wand", "Robe")),
     PRIEST(10, 8, List.of("Mase", "Chainmail Armor"));
 
     private int baseHealth;
