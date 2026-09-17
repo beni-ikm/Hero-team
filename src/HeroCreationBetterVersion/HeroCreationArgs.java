b814a31a1a75002fb632ce5170d77164e47ee720
Manuel Marcelo Baechle <manuelmarcelobachle@gmail.com>
2026-09-17 17:11:38 -0400

    Return to primitive int (stats)

--- src/HeroCreationBetterVersion/HeroCreationArgs.java
+++ src/HeroCreationBetterVersion/HeroCreationArgs.java
@@ -1,7 +1,7 @@
 package HeroCreationBetterVersion;
 // Cette classe contient seulement les arguments (voir parameter object ppw)
 public record HeroCreationArgs(String name, Species species, Class job,
-                               Stats health, Stats strength, Stats dexterity,
-                               Stats intelligence, Stats constitution,
-                               Stats charisma, Stats wisdom) {
+                               int health, int strength, int dexterity,
+                               int intelligence, int constitution,
+                               int charisma, int wisdom) {
 }
